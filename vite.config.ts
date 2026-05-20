b7ef7ec83bd012510746c5eb6c0ea8d7c2a97848
soatra <soatra206.infinity@gmail.com>
2026-05-20 16:47:47 +0700

    index style to index.js

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import tailwindcss from '@tailwindcss/vite';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 export default defineConfig({
   plugins: [
     react(),
     svgr(),
     tailwindcss(),
+    libInjectCss(),
   ],
   build: {
     emptyOutDir: false,
-    cssCodeSplit: false,
     lib: {
       entry: 'src/lib/index.ts',
       name: 'ReactMiniBag',
